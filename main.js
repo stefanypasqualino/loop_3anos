7b1e84f1814edb21bc740835a12d62d9331a969f
stefany pasqualino <106812995+stefanypasqualino@users.noreply.github.com>
2024-04-16 11:40:12 -0300

    novo

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Qual é o filme?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "moranguinho") {
+      alert("Isso mesmo! O Filme é moranguinho!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
